63738332692b4f614b3e8769c6381b49b2882496
eodyd9684 <eodyd9684@naver.com>
2025-03-25 15:28:02 +0900

    native 폴더 생성 및 기본 셋팅

---
+++ farmseye_native/app/index.jsx
@@ -0,0 +1,15 @@
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import Task from '../components/Task'
+
+const index = () => {
+  return (
+    <SafeAreaView>
+      <Task/>
+    </SafeAreaView>
+  )
+}
+
+export default index
+
+const styles = StyleSheet.create({})
